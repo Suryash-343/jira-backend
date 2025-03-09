6b4865b5421ce21f22c3ba47d9754d1de1c8bc64
Suryash Kumar Jha | IT | Pramericalife <P037343@pramericalife.in>
2025-03-09 12:21:16 +0530

    current changes

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,12 +1,22 @@
 // export class CreateTaskDto {
 
+import { Optional } from "@nestjs/common";
+import { Transform } from "class-transformer";
+
 // }
 export class CreateTaskDto {
+
+
   title: string;
   priority: number;
   description: string;
   status: string;
+  @Transform(value => {
+    console.log(value, '----valueuuuu')
+    return new Date(String(value))
+})
   dueDate: Date;
+  
   createdBy: string;
   assignedTo: string;
   
