7cde79ad59a08877a1de79a4cdf616e5b9a57b16
Suryash Kumar Jha | IT | Pramericalife <P037343@pramericalife.in>
2025-03-09 12:29:12 +0530

    Validtion at global level added

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -2,22 +2,43 @@
 
 import { Optional } from "@nestjs/common";
 import { Transform } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional } from "class-validator";
 
 // }
 export class CreateTaskDto {
 
-
+  @IsNotEmpty()
   title: string;
+
+  @IsOptional()
+  @IsNumber()
   priority: number;
+
+  @IsOptional()
   description: string;
+
+  @IsNotEmpty()
   status: string;
+
+  @IsOptional()
   @Transform(value => {
     console.log(value, '----valueuuuu')
     return new Date(String(value))
 })
   dueDate: Date;
   
+  @IsNotEmpty()
   createdBy: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  createdByEmail: string;
+
+  @IsNotEmpty()
   assignedTo: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  assignedToEmail: string;
   
 }
