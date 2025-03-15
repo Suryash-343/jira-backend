0314823ea19e0f8dbef53ea99c7de04399da5b1e
Suryash Kumar Jha | IT | Pramericalife <P037343@pramericalife.in>
2025-03-15 12:45:16 +0530

    prefix api added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   console.log('app is running on port :', process.env.PORT ?? 3000);
   await app.listen(process.env.PORT ?? 3000);
