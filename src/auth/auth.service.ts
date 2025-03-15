1c86f55ea3130dbc9ee1b8109269f17fdb741322
Suryash Kumar Jha | IT | Pramericalife <P037343@pramericalife.in>
2025-03-15 18:22:33 +0530

    Unique user register and login different status code error added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,44 +13,46 @@ export class AuthService {
   constructor(
     @InjectRepository(User) private authRepository: Repository<User>,
     private jwtService: JwtService,
-  ) {}
-  public async create(createUserDto: CreateUserDto) { 
+  ) { }
+  public async create(createUserDto: CreateUserDto) {
+    const user = await this.authRepository.findOne({ where: { email: createUserDto.email } });
+    if (user)
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+
     const reqPassword = createUserDto.password;
-    const hashedpassword= await hashString(reqPassword);
-    createUserDto.password= hashedpassword
+    const hashedpassword = await hashString(reqPassword);
+    createUserDto.password = hashedpassword
+    createUserDto.role = createUserDto.role ? createUserDto.role : 'user'
+
     await this.authRepository.save(createUserDto);
     return {
-        message: 'User created successfully',
-        status: 201,
+      message: 'User created successfully',
+      status: 201,
     }
   }
-  public async login(loginAuthDto: LoginUserDto) { 
+  public async login(loginAuthDto: LoginUserDto) {
     const reqEmail = loginAuthDto.email;
     const reqPassword = loginAuthDto.password;
+    console.log(reqEmail, reqPassword, '---REQ')
+    const user = await this.authRepository.findOne({ where: { email: reqEmail } });
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
 
-    const user= await this.authRepository.findOne({where:{email: reqEmail}});
-    const isPasswordSame= await isHashSame(reqPassword, user.password);
+    }
+    const isPasswordSame = await isHashSame(reqPassword, user.password);
     // console.log(user, '---USER')
-    if(!user ){
-        return {
-            message: 'User not found',
-            status: 404,
-        }
-    }
-    if(user && !isPasswordSame){
-        return {
-            message: 'Password is incorrect',
-            status: 404,
-        }
+    if (user && !isPasswordSame) {
+      throw new HttpException('Password is incorrect', HttpStatus.NOT_FOUND);
+
     }
 
-    const {password,createdAt, updatedAt, ...payload}= user
-    const access_token=  await this.jwtService.signAsync(payload)
+    const { password, createdAt, updatedAt, ...payload } = user
+    const access_token = await this.jwtService.signAsync(payload)
 
     return {
-        access_token,
-        message: 'User Logged In successfully',
-        status: 201,
+      access_token,
+      message: 'User Logged In successfully',
+      status: 201,
     }
   }
 
@@ -63,10 +65,10 @@ export class AuthService {
   }
 
   async update(id: string, updateUserDto: Partial<UpdateUserDto>) {
-      await this.authRepository.update(id, updateUserDto);
-      return await this.authRepository.findOne({ where: { id } });
-    }
-    
+    await this.authRepository.update(id, updateUserDto);
+    return await this.authRepository.findOne({ where: { id } });
+  }
+
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
